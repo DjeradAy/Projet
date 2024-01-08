252e9e673a6000fae6e0b254e7bb29221f82a191
DjeradAy <153507923+DjeradAy@users.noreply.github.com>
2024-01-08 14:50:48 +0100

    Delete Fonction Tvi.py

--- Fonction Tvi.py
+++
@@ -1,7 +0,0 @@
-Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:19:08) [MSC v.1500 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import math
-f(x) == x** - 8*ln(x)
-for x in range [1,2] :
-	
->>> 
